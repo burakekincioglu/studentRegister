e3b2981552d9ed9f39e14ff9c6cfb817a6f6d94a
burakekincioglu <burakekincioglu93@gmail.com>
2022-01-21 11:28:57 +0300

    reducer added to store

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-
-
+import {Provider} from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers'
 
 const App = () => {
 
   return (
+    <Provider store={createStore(reducers)}>
     <View>
       <Text> Student Register App </Text>
     </View>
+    </Provider>
   );
 };
 
